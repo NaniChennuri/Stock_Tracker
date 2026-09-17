32e02551165ba0096df5623b62a1bf158b35f755
Sai Madan Chennuri <chennurimadan@gmail.com>
2026-09-17 15:36:41 +0530

    Public

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   github_user: 'NaniChennuri',
-  github_repo: 'Stock_Tracker',
+  github_repo: 'stock-data',
   data_file:   'data.json',
   app_name:    'Hanuman Enterprises',
   pin:         '0130',
